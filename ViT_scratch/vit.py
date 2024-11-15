e8ca6a32dd7abefc8959c1a0941564f9addc1de3
jchanxtarov <ryotaro.shimizu@zozo.com>
2024-11-15 11:46:15 +0900

    [fix] bug in trainer

--- ViT_scratch/vit.py
+++ ViT_scratch/vit.py
@@ -12,7 +12,17 @@ class NewGELUActivation(nn.Module):
     """
 
     def forward(self, input):
-        return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
+        return (
+            0.5
+            * input
+            * (
+                1.0
+                + torch.tanh(
+                    math.sqrt(2.0 / math.pi)
+                    * (input + 0.044715 * torch.pow(input, 3.0))
+                )
+            )
+        )
 
 
 class PatchEmbeddings(nn.Module):
@@ -30,19 +40,36 @@ class PatchEmbeddings(nn.Module):
         # Calculate the number of patches from the image size and patch size
         self.num_patches = (self.image_size // self.patch_size) ** 2
         # Create a projection layer to convert the image into patches
-          # The layer projects each patch into a vector of size hidden_size
+        # The layer projects each patch into a vector of size hidden_size
 
         # self.projection = nn.Conv2d(self.num_channels, self.hidden_size, kernel_size=self.patch_size, stride=self.patch_size)
 
-        self.projections = nn.ModuleList([
-            nn.Conv2d(self.num_channels, self.hidden_size, kernel_size=self.patch_size, stride=self.patch_size),
-            nn.Conv2d(self.num_channels_forDepth, self.hidden_size, kernel_size=self.patch_size, stride=self.patch_size),
-            nn.Conv2d((self.num_channels+self.num_channels_forDepth), self.hidden_size, kernel_size=self.patch_size, stride=self.patch_size)
-        ])
+        self.projections = nn.ModuleList(
+            [
+                nn.Conv2d(
+                    self.num_channels,
+                    self.hidden_size,
+                    kernel_size=self.patch_size,
+                    stride=self.patch_size,
+                ),
+                nn.Conv2d(
+                    self.num_channels_forDepth,
+                    self.hidden_size,
+                    kernel_size=self.patch_size,
+                    stride=self.patch_size,
+                ),
+                nn.Conv2d(
+                    (self.num_channels + self.num_channels_forDepth),
+                    self.hidden_size,
+                    kernel_size=self.patch_size,
+                    stride=self.patch_size,
+                ),
+            ]
+        )
 
     def forward(self, x, Isdepth=False):
         # (batch_size, num_channels, image_size, image_size) -> (batch_size, num_patches, hidden_size)
-        #x = self.projection(x)
+        # x = self.projection(x)
         projection_layer = self.projections[1] if Isdepth else self.projections[0]
 
         # Apply the selected projection layer
@@ -56,7 +83,7 @@ class Embeddings(nn.Module):
     """
     Combine the patch embeddings with the class token and position embeddings.
     """
-        
+
     def __init__(self, config):
         super().__init__()
         self.config = config
@@ -67,8 +94,9 @@ class Embeddings(nn.Module):
         self.cls_token = nn.Parameter(torch.randn(1, 1, config["hidden_size"]))
         # Create position embeddings for the [CLS] token and the patch embeddings
         # Add 1 to the sequence length for the [CLS] token
-        self.position_embeddings = \
-            nn.Parameter(torch.randn(1, self.patch_embeddings.num_patches + 1, config["hidden_size"]))
+        self.position_embeddings = nn.Parameter(
+            torch.randn(1, self.patch_embeddings.num_patches + 1, config["hidden_size"])
+        )
         self.dropout = nn.Dropout(config["hidden_dropout_prob"])
 
     def forward(self, x, Isdepth=False):
@@ -93,6 +121,7 @@ class AttentionHead(nn.Module):
     This module is used in the MultiHeadAttention module.
 
     """
+
     def __init__(self, hidden_size, attention_head_size, dropout, bias=True):
         super().__init__()
         self.hidden_size = hidden_size
@@ -103,7 +132,7 @@ class AttentionHead(nn.Module):
         self.value = nn.Linear(hidden_size, attention_head_size, bias=bias)
 
         self.dropout = nn.Dropout(dropout)
-    
+
     def forward(self, x):
         # Project the input into query, key, and value
         # The same input is used to generate the query, key, and value,
@@ -145,7 +174,7 @@ class MultiHeadAttention(nn.Module):
                 self.hidden_size,
                 self.attention_head_size,
                 config["attention_probs_dropout_prob"],
-                self.qkv_bias
+                self.qkv_bias,
             )
             self.heads.append(head)
         # Create a linear layer to project the attention output back to the hidden size
@@ -157,7 +186,9 @@ class MultiHeadAttention(nn.Module):
         # Calculate the attention output for each attention head
         attention_outputs = [head(x) for head in self.heads]
         # Concatenate the attention outputs from each attention head
-        attention_output = torch.cat([attention_output for attention_output, _ in attention_outputs], dim=-1)
+        attention_output = torch.cat(
+            [attention_output for attention_output, _ in attention_outputs], dim=-1
+        )
         # Project the concatenated attention output back to the hidden size
         attention_output = self.output_projection(attention_output)
         attention_output = self.output_dropout(attention_output)
@@ -165,7 +196,9 @@ class MultiHeadAttention(nn.Module):
         if not output_attentions:
             return (attention_output, None)
         else:
-            attention_probs = torch.stack([attention_probs for _, attention_probs in attention_outputs], dim=1)
+            attention_probs = torch.stack(
+                [attention_probs for _, attention_probs in attention_outputs], dim=1
+            )
             return (attention_output, attention_probs)
 
 
@@ -185,7 +218,9 @@ class FasterMultiHeadAttention(nn.Module):
         # Whether or not to use bias in the query, key, and value projection layers
         self.qkv_bias = config["qkv_bias"]
         # Create a linear layer to project the query, key, and value
-        self.qkv_projection = nn.Linear(self.hidden_size, self.all_head_size * 3, bias=self.qkv_bias)
+        self.qkv_projection = nn.Linear(
+            self.hidden_size, self.all_head_size * 3, bias=self.qkv_bias
+        )
         self.attn_dropout = nn.Dropout(config["attention_probs_dropout_prob"])
         # Create a linear layer to project the attention output back to the hidden size
         # In most cases, all_head_size and hidden_size are the same
@@ -201,9 +236,24 @@ class FasterMultiHeadAttention(nn.Module):
         query, key, value = torch.chunk(qkv, 3, dim=-1)
         # Resize the query, key, and value to (batch_size, num_attention_heads, sequence_length, attention_head_size)
         batch_size, sequence_length, _ = query.size()
-        query = query.view(batch_size, sequence_length, self.num_attention_heads, self.attention_head_size).transpose(1, 2)
-        key = key.view(batch_size, sequence_length, self.num_attention_heads, self.attention_head_size).transpose(1, 2)
-        value = value.view(batch_size, sequence_length, self.num_attention_heads, self.attention_head_size).transpose(1, 2)
+        query = query.view(
+            batch_size,
+            sequence_length,
+            self.num_attention_heads,
+            self.attention_head_size,
+        ).transpose(1, 2)
+        key = key.view(
+            batch_size,
+            sequence_length,
+            self.num_attention_heads,
+            self.attention_head_size,
+        ).transpose(1, 2)
+        value = value.view(
+            batch_size,
+            sequence_length,
+            self.num_attention_heads,
+            self.attention_head_size,
+        ).transpose(1, 2)
         # Calculate the attention scores
         # softmax(Q*K.T/sqrt(head_size))*V
         attention_scores = torch.matmul(query, key.transpose(-1, -2))
@@ -215,9 +265,11 @@ class FasterMultiHeadAttention(nn.Module):
         # Resize the attention output
         # from (batch_size, num_attention_heads, sequence_length, attention_head_size)
         # To (batch_size, sequence_length, all_head_size)
-        attention_output = attention_output.transpose(1, 2) \
-                                           .contiguous() \
-                                           .view(batch_size, sequence_length, self.all_head_size)
+        attention_output = (
+            attention_output.transpose(1, 2)
+            .contiguous()
+            .view(batch_size, sequence_length, self.all_head_size)
+        )
         # Project the attention output back to the hidden size
         attention_output = self.output_projection(attention_output)
         attention_output = self.output_dropout(attention_output)
@@ -266,8 +318,9 @@ class Block(nn.Module):
 
     def forward(self, x, output_attentions=False):
         # Self-attention
-        attention_output, attention_probs = \
-            self.attention(self.layernorm_1(x), output_attentions=output_attentions)
+        attention_output, attention_probs = self.attention(
+            self.layernorm_1(x), output_attentions=output_attentions
+        )
         # Skip connection
         x = x + attention_output
         # Feed-forward network
@@ -298,7 +351,9 @@ class Encoder(nn.Module):
         # Calculate the transformer block's output for each block
         all_attentions = []
         for block in self.blocks:
+            print("[test] here: x, attention_probs = block(")
             x, attention_probs = block(x, output_attentions=output_attentions)
+            print(f"[test] x: {x.shape}")
             if output_attentions:
                 all_attentions.append(attention_probs)
         # Return the encoder's output and the attention probabilities (optional)
@@ -333,7 +388,9 @@ class ViTForClassfication(nn.Module):
         embedding_output_image = self.embedding(x, Isdepth=Isdepth)
 
         # Calculate the encoder's output
-        encoder_output, all_attentions = self.encoder(embedding_output_image, output_attentions=output_attentions)
+        encoder_output, all_attentions = self.encoder(
+            embedding_output_image, output_attentions=output_attentions
+        )
         # Calculate the logits, take the [CLS] token's output as features for classification
         logits = self.classifier(encoder_output[:, 0, :])
         # Return the logits and the attention probabilities (optional)
@@ -341,10 +398,12 @@ class ViTForClassfication(nn.Module):
             return (logits, None)
         else:
             return (logits, all_attentions)
-    
+
     def _init_weights(self, module):
         if isinstance(module, (nn.Linear, nn.Conv2d)):
-            torch.nn.init.normal_(module.weight, mean=0.0, std=self.config["initializer_range"])
+            torch.nn.init.normal_(
+                module.weight, mean=0.0, std=self.config["initializer_range"]
+            )
             if module.bias is not None:
                 torch.nn.init.zeros_(module.bias)
         elif isinstance(module, nn.LayerNorm):
@@ -363,7 +422,8 @@ class ViTForClassfication(nn.Module):
                 std=self.config["initializer_range"],
             ).to(module.cls_token.dtype)
 
-class ViTForClassfication_method2(nn.Module):
+
+class DepthViT(nn.Module):
     """
     The ViT model for classification.
     """
@@ -383,28 +443,37 @@ class ViTForClassfication_method2(nn.Module):
         # Initialize the weights
         self.apply(self._init_weights)
 
-    def forward(self, x1, x2, output_attentions=False, Isdepth=False):
+    def forward(self, img, dpt, output_attentions=False, Isdepth=False):
         # Calculate the embedding output
-        embedding_output_image = self.embedding(x1, Isdepth=Isdepth)
-        #print(f"emd_image: {embedding_output_image.shape}")
-        embedding_output_depth = self.embedding(x2, Isdepth=True)
-        #print(f"emd_depth: {embedding_output_depth.shape}")
-        embedding_output_fusion = torch.cat((embedding_output_image, embedding_output_depth),dim=1)
-        #print(f"emd_fusion: {embedding_output_fusion.shape}")
+        print("[test] here: embedding_output_image = self.embedding")
+        embedding_output_image = self.embedding(img, Isdepth=Isdepth)
+        print(f"[test] emd_image: {embedding_output_image.shape}")
+        embedding_output_depth = self.embedding(dpt, Isdepth=True)
+        print(f"[test] emd_depth: {embedding_output_depth.shape}")
+        embedding_output_fusion = torch.cat(
+            (embedding_output_image, embedding_output_depth), dim=1
+        )
+        print(f"[test] emd_fusion: {embedding_output_fusion.shape}")
 
         # Calculate the encoder's output
-        encoder_output, all_attentions = self.encoder(embedding_output_fusion, output_attentions=output_attentions)
+        encoder_output, all_attentions = self.encoder(
+            embedding_output_fusion, output_attentions=output_attentions
+        )
+        print(f"[test] encoder_output: {encoder_output.shape}")
         # Calculate the logits, take the [CLS] token's output as features for classification
         logits = self.classifier(encoder_output[:, 0, :])
+        print(f"[test] logits: {logits.shape}")
         # Return the logits and the attention probabilities (optional)
         if not output_attentions:
             return (logits, None)
         else:
             return (logits, all_attentions)
-    
+
     def _init_weights(self, module):
         if isinstance(module, (nn.Linear, nn.Conv2d)):
-            torch.nn.init.normal_(module.weight, mean=0.0, std=self.config["initializer_range"])
+            torch.nn.init.normal_(
+                module.weight, mean=0.0, std=self.config["initializer_range"]
+            )
             if module.bias is not None:
                 torch.nn.init.zeros_(module.bias)
         elif isinstance(module, nn.LayerNorm):
